2bfb8697fc0927e80763c99b7eecdf1993953704
Gift Banda <gift.banda@standardbank.co.za>
2022-04-16 18:24:44 +0200

    updated

--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
 	"io/ioutil"
 	"log"
+	"strings"
 	"time"
 )
 
 //Claims can have user id.. etc for Identification purpose
 type Claims struct {
-	Userid     string `json:"userid"`
+	UserId     string `json:"user_id"`
 	Authstatus string `json:"authstatus"`
-	Role       string `json:"userrole"`
+	Role       string `json:"role"`
 	Exp        int64  `json:"exp"`
 	jwt.StandardClaims
 }
@@ -24,13 +26,50 @@ var (
 )
 
 func init() {
-	privateKey, err = ioutil.ReadFile("certs/jwt-private.pem") //used for generating the token
+	privateKey, err = ioutil.ReadFile("certs/chain.key")
 	errLog(err)
-	publicKey, err = ioutil.ReadFile("certs/jwt-public.pem") //used for validating the token
+	publicKey, err = ioutil.ReadFile("certs/chain.pem")
 	errLog(err)
 }
 
-func GenerateToken(userId string, authstatus string, userrole string) (interface{}, error) {
+func AuthHandler(authRoles ...string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.Request.Header.Get("Authorization")
+		// Check if token in correct format
+		// ie eyJHBxxxxxxxxx
+		b := "eyJhb"
+		if !strings.Contains(token, b) {
+			c.JSON(403, gin.H{"message": "Your request is not authorized"})
+			c.Abort()
+			return
+		}
+
+		t := strings.Split(token, b)
+		if len(t) < 2 {
+			c.JSON(403, gin.H{"message": "An authorization token was not supplied"})
+			c.Abort()
+			return
+		}
+
+		claims, err := ValidateToken(token)
+		if err != nil {
+			fmt.Println(err)
+			c.JSON(403, gin.H{"message": "Invalid authorization token"})
+			c.Abort()
+			return
+		}
+
+		claimValue := claims.(jwt.MapClaims) //
+
+		//fmt.Println(claimValue["user_id"], claimValue["authstatus"])
+		c.Set("userId", claimValue["user_id"])
+		c.Set("authstatus", claimValue["authstatus"])
+		c.Set("role", claimValue["role"])
+		c.Next()
+	}
+}
+
+func GenerateToken(userId string, authstatus string, userrole string) (string, error) {
 	privateRSA, err := jwt.ParseRSAPrivateKeyFromPEM(privateKey)
 	errLog(err)
 
@@ -47,11 +86,10 @@ func GenerateToken(userId string, authstatus string, userrole string) (interface
 	return token, err
 }
 
-func ValidateToken(inToken interface{}) (interface{}, error) {
-	publicRSA, err := jwt.ParseRSAPublicKeyFromPEM(publicKey)
-	errLog(err)
+func ValidateToken(inToken string) (interface{}, error) {
+	publicRSA := ParseRSAPublicKey()
 
-	token, err := jwt.Parse(inToken.(string), func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.Parse(inToken, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
@@ -59,28 +97,37 @@ func ValidateToken(inToken interface{}) (interface{}, error) {
 	})
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		return claims, nil //fmt.Println("Valid token")
+		return claims, nil // fmt.Println("Valid token")
+
 	} else {
-		return nil, err //fmt.Println("Invalid token")
+		return nil, err // fmt.Println("Invalid token")
 	}
 }
 
-func main() {
-	token, err := GenerateToken("giftmbanda@gmail.com", "true", "admin") //generate token
+func GetClaim(jwtToken string, claimreq string) interface{} {
+	claims, err := ValidateToken(jwtToken) // validate token
 	errLog(err)
-
-	fmt.Println(token)
-
-	claims, err := ValidateToken(token) // validate token
-	errLog(err)
-
-	claimValue := claims.(jwt.MapClaims) 
-	// extract token claims
-	fmt.Println(claimValue["userid"], claimValue["authstatus"],claimValue["userrole"],claimValue["exp"],)
+	claimValue := claims.(jwt.MapClaims)
+	return claimValue[claimreq] // return claims
 }
 
 func errLog(err error) {
 	if err != nil {
-		log.Fatal("Error:", err.Error())
+		log.Fatal("Error: ", err.Error())
 	}
+}
+
+func ParseRSAPublicKey() interface{} {
+	publicRSA, err := jwt.ParseRSAPublicKeyFromPEM(publicKey)
+	errLog(err)
+	return publicRSA
+}
+
+func main() {
+	token, err := GenerateToken("giftmbanda@gmail.com", "true", "admin") //generate token
+
+	errLog(err)
+	fmt.Println(token)
+
+	fmt.Println(GetClaim(token, "user_id"), GetClaim(token, "authstatus"), GetClaim(token, "role"), GetClaim(token, "exp"))
 }
